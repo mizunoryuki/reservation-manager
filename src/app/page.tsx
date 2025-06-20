bbaa3e1420da4948149953da768ff5ed839fd575
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-06-20 06:25:03 +0000

    ログインコンポーネント作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import { Sidebar } from "@/components/sidebar/page";
-import { Contents } from "@/components/contents/page";
 import { Login } from "@/components/login/page";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <Sidebar />
-      <Contents />
       <Login />
     </div>
   );
--- src/components/login/page.tsx
+++ src/components/login/page.tsx
@@ -1,6 +1,40 @@
-
+"use client"
+import { useState } from "react"
+import styles from "./page.module.css"
 export const Login = () => {
+	const [email,setEmail] = useState("")
+	const [password,setPassword] = useState("")
+
+	const handleRegister = () => {
+		console.log("新規登録")
+		//新規登録画面に遷移
+	}
+
+	const handleLogin = () => {
+		console.log("ログイン")
+		//認証
+	}
+
 	return(
-		<p>Login</p>
+<div className={styles.loginForm}>
+  <h2>ログイン</h2>
+
+  <div className={styles.formFields}>
+    <div className={styles.formField}>
+      <label htmlFor="email">E-mail</label>
+      <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    </div>
+    <div className={styles.formField}>
+      <label htmlFor="password">パスワード</label>
+      <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+    </div>
+  </div>
+
+  <div className={styles.formActions}>
+    <button onClick={handleLogin}>ログイン</button>
+    <button onClick={handleRegister}>新規登録</button>
+  </div>
+</div>
+
 	)
 }
