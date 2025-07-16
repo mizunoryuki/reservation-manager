2e540447afbe4c65486cd5f08ea45feb63e37dca
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-07-16 16:55:49 +0900

    管理者の店舗登録機能

--- src/app/(main)/admin/stores/new/page.tsx
+++ src/app/(main)/admin/stores/new/page.tsx
@@ -1,7 +1,14 @@
 import AdminRegistration from "@/components/admin/AdminResitration/page";
+import { getAccessTokenFromCookie } from "@/lib/getCookie";
+import { redirect } from "next/navigation";
 
-export default function NewStore () {
-	return (
-		<AdminRegistration />
-	)
-} 
+export default async function NewStore() {
+  const token = await getAccessTokenFromCookie();
+  if (!token) {
+    redirect("/");
+  }
+
+  console.log(token);
+
+  return <AdminRegistration token={token}/>;
+}
--- src/components/admin/AdminResitration/page.tsx
+++ src/components/admin/AdminResitration/page.tsx
@@ -5,21 +5,69 @@ import styles from "./page.module.css";
 const START_HOUR = 8;
 const FINISH_HOUR = 22;
 
-export default function AdminRegistration() {
+type Props = {
+  token: string;
+};
+
+export default function AdminRegistration({ token }: Props) {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
   const [details, setDetails] = useState("");
-  const [selectedtime, setSelectedTime] = useState(`0${START_HOUR + 1}`);
+  const [startTime, setStartTime] = useState(
+    `${START_HOUR.toString().padStart(2, "0")}:00`
+  );
+  const [endTime, setEndTime] = useState(
+    `${(START_HOUR + 1).toString().padStart(2, "0")}:00`
+  );
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await fetch("http://localhost:4000/admin/stores", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({
+        name: name,
+        address: address,
+        details: details,
+        business_start_time: startTime,
+        business_end_time: endTime,
+      }),
+    });
+
+    if (res.ok) {
+      alert("店舗登録成功！");
+    } else {
+      const error = await res.text();
+      console.error("登録失敗:", error);
+      alert("店舗登録失敗");
+    }
+  };
 
   return (
     <div className={styles.container}>
-      <form className={styles.registerBox}>
+      <form className={styles.registerBox} onSubmit={handleSubmit}>
         <h2>店舗の新規登録</h2>
         <div className={styles.formBox}>
           <label>店舗名</label>
-          <input type="text" placeholder="〇〇商店" />
+          <input
+            type="text"
+            placeholder="〇〇商店"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
         <div className={styles.formBox}>
           <label>住所</label>
-          <input type="text" placeholder="東京都渋谷区〇〇" />
+          <input
+            type="text"
+            placeholder="東京都渋谷区〇〇"
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
         </div>
         <div className={styles.formBox}>
           <label>内容</label>
@@ -36,38 +84,45 @@ export default function AdminRegistration() {
           <div className={styles.timeContainer}>
             <div className={styles.timeBox}>
               <label>開店時間</label>
-              <select onChange={(e) => setSelectedTime(e.target.value)}>
-                {Array.from(
-                  { length: FINISH_HOUR - START_HOUR },
-                  (_, index) => {
-                    const time = START_HOUR + index;
-                    const paddedTime = time.toString().padStart(2, "0");
-                    const paddedTimeNext = (time + 1)
-                      .toString()
-                      .padStart(2, "0");
-
-                    return (
-                      <option key={index} value={paddedTimeNext}>
-                        {paddedTime}:00
-                      </option>
-                    );
-                  }
-                )}
+              <select
+                onChange={(e) => setStartTime(e.target.value)}
+                value={startTime}
+              >
+                {Array.from({ length: FINISH_HOUR - START_HOUR }, (_, i) => {
+                  const time = START_HOUR + i;
+                  const padded = time.toString().padStart(2, "0");
+                  return (
+                    <option key={padded} value={`${padded}:00`}>
+                      {padded}:00
+                    </option>
+                  );
+                })}
               </select>
             </div>
             <div className={styles.kara}>
               <p>から</p>
             </div>
             <div className={styles.timeBox}>
-              <label>開店時間</label>
-              <p>{selectedtime}:00</p>
+              <label>閉店時間</label>
+              <select
+                onChange={(e) => setEndTime(e.target.value)}
+                value={endTime}
+              >
+                {Array.from({ length: FINISH_HOUR - START_HOUR }, (_, i) => {
+                  const time = START_HOUR + i + 1;
+                  const padded = time.toString().padStart(2, "0");
+                  return (
+                    <option key={padded} value={`${padded}:00`}>
+                      {padded}:00
+                    </option>
+                  );
+                })}
+              </select>
             </div>
           </div>
         </div>
         <div className={styles.buttonBox}>
-          <button type="submit" onClick={() => console.log("登録")}>
-            登録
-          </button>
+          <button type="submit">登録</button>
         </div>
       </form>
     </div>
