d7d86d248ca88233eecf2f64b1a3f6599bfc6d21
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2025-07-16 14:17:53 +0900

    ユーザ登録API接続

--- src/components/signup/page.tsx
+++ src/components/signup/page.tsx
@@ -1,39 +1,73 @@
-"use client"
-import { useState } from "react"
-import styles from "./page.module.css"
+"use client";
+import { useState } from "react";
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
 export const SignUp = () => {
-	const [name,setName] = useState<string>("")
-	const [email,setEmail] = useState<string>("")
-	const [password,setPassword] = useState<string>("")
+  const router = useRouter();
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-	const handleRegister = () => {
-		console.log("新規登録")
-		//登録処理
-	}
+  const handleRegister = async () => {
+    //登録処理
+    const res: Response = await fetch("http://localhost:4000/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify({ Name: name, Email: email, Password: password }),
+    });
 
-	return(
-<div className={styles.loginForm}>
-  <h2>新規登録</h2>
+    if (!res.ok) {
+      console.log("failed to signup");
+    } else {
+      const { message } = await res.json();
+      console.log(message);
+      router.push("/");
+    }
+  };
 
-  <div className={styles.formFields}>
-	    <div className={styles.formField}>
-      <label htmlFor="name">Name</label>
-      <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} placeholder="山田太郎"/>
+  return (
+    <div className={styles.loginForm}>
+      <h2>新規登録</h2>
+
+      <div className={styles.formFields}>
+        <div className={styles.formField}>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="山田太郎"
+          />
+        </div>
+        <div className={styles.formField}>
+          <label htmlFor="email">E-mail</label>
+          <input
+            id="email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="example@example.com"
+          />
+        </div>
+        <div className={styles.formField}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="6文字以上"
+          />
+        </div>
+      </div>
+
+      <div className={styles.formActions}>
+        <button onClick={handleRegister}>新規登録</button>
+      </div>
     </div>
-    <div className={styles.formField}>
-      <label htmlFor="email">E-mail</label>
-      <input id="email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="example@example.com"/>
-    </div>
-    <div className={styles.formField}>
-      <label htmlFor="password">Password</label>
-      <input id="password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="6文字以上"/>
-    </div>
-  </div>
-
-  <div className={styles.formActions}>
-    <button onClick={handleRegister}>新規登録</button>
-  </div>
-</div>
-
-	)
-}
+  );
+};
